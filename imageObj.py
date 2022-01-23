e4f322c26741bdafbcd7908e20618f246a680ed1
Stephanie-programmer <739781205@qq.com>
2022-01-23 08:40:44 -0800

    show final image without rotation

--- imageObj.py
+++ imageObj.py
@@ -1,7 +1,9 @@
 class ImageObj:
     rotation_offset = 20
+    position_offset = 30
 
-    def __init__(self, image, pos, rotation, direction):
+    def __init__(self, image, pos, rotation, direction, hidden=False):
+        self.hidden = hidden
         self.image = image
         self.pos = pos
         self.rotation = rotation
--- main.py
+++ main.py
@@ -3,12 +3,43 @@ from imageObj import *
 
 SCREEN_SIZE = (int(800 * 1.5), int(450 * 1.5))
 BULL_BG_COLOR = (0.8471 * 255, 0.22157 * 255, 0.1765 * 255)
+LION_BG_COLOR = (0.8471 * 255, 0.1020 * 255, 0.2039 * 255)
 
 
 def draw():
-    screen.fill(BULL_BG_COLOR)
-    screen.blit(tiger_l.image, tiger_l.pos)
-    screen.blit(tiger_r.image, tiger_r.pos)
+    if lion.hidden:
+        screen.fill(BULL_BG_COLOR)
+        screen.blit(tiger_l.image, tiger_l.pos)
+        screen.blit(tiger_r.image, tiger_r.pos)
+    else:
+        screen.fill(LION_BG_COLOR)
+        screen.blit(lion.image, lion.pos)  # draw lion in the middle
+        draw_greeting()
+        draw_tiger_l_rot()
+        draw_tiger_r_rot()
+
+
+def draw_tiger_r_rot():
+    screen.blit(tiger_r.image, tiger_r.image.get_rect(
+        center=tiger_r.image.get_rect(topleft=(
+            SCREEN_SIZE[0] - tiger_r.image.get_width() - tiger_r.position_offset,
+            (SCREEN_SIZE[1] - tiger_r.image.get_height()) / 2)).center).topleft)
+
+
+def draw_tiger_l_rot():
+    screen.blit(tiger_l.image, tiger_l.image.get_rect(
+        center=tiger_l.image.get_rect(topleft=(tiger_l.position_offset,
+                                               (SCREEN_SIZE[
+                                                    1] - tiger_l.image.get_height()) / 2)).center).topleft)
+
+
+def draw_greeting():
+    font = pygame.font.Font('YSHaoShenTi-2.ttf', 100)
+    greeting = "新春快乐"
+    font_color = (255, 207, 64)
+    greeting_pos = (SCREEN_SIZE[0] / 2.7, SCREEN_SIZE[1] * 3 / 4)
+    greeting = font.render(greeting, True, font_color)
+    screen.blit(greeting, greeting_pos)
 
 
 def create_tiger_l():
@@ -27,6 +58,13 @@ def create_tiger_r():
     return ImageObj(tiger_r_image, tiger_r_pos, 0, "l")
 
 
+def create_lion():
+    lion_image = pygame.image.load("lion_middle.jpg")
+    lion_image = pygame.transform.scale(lion_image, (lion_image.get_width() // 3, lion_image.get_height() // 3))
+    lion_pos = ((SCREEN_SIZE[0] - lion_image.get_width()) / 2, (SCREEN_SIZE[1] - lion_image.get_height()) / 8)
+    return ImageObj(lion_image, lion_pos, 0, "l", True)
+
+
 def update(t_elaps):
     tiger_r.pos = (tiger_r.pos[0] - t_elaps / 10, tiger_r.pos[1])
     tiger_l.pos = (tiger_l.pos[0] + t_elaps / 10, tiger_l.pos[1])
@@ -37,6 +75,7 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 t_pre = pygame.time.get_ticks()
 tiger_l = create_tiger_l()
 tiger_r = create_tiger_r()
+lion = create_lion()
 running = True
 while running:
     for event in pygame.event.get():
